449ec82d80c394caae2de5d11959f793daffae60
Scott Bronson <brons_git@rinspin.com>
2025-12-28 17:10:49 -0600

    add Twin Cities

--- Nether Map.py
+++ Nether Map.py
@@ -9,8 +9,10 @@ VILLAGES = {
     # and will be displayed in red, False means the location will be blue.
     # X is left-right with negative to the west, Z is up-down with negative to the north.
     'The Harbor': (62, 40, -52, True),
+    'The Armory': (39, 52, 31, True),
     'Oslo': (-435, 74, -276, True),
     'Alpine Village': (-281, 100, -90, True),
+    'Sculk': (-337, 33, -105, True),
     'Snowy Village': (-228, 102, -44, True),
     'Zombie Village': (-295, 99, 2, True),
     'Hill Village': (-162, 70, 75, True),
@@ -28,6 +30,9 @@ VILLAGES = {
     'Mansion': (-593, 73, -144, True),
     'Wuwey': (-749, 77, -192, True),
     'Pitchfork': (-261, 61, 216, True),
+    'Coarsegold': (102, 60, 11, True),
+    'Pillville': (83, 66, 114, True),
+    'Twin Cities': (316, 78, 549, True),
 }
 
 # paths between locations
@@ -108,115 +113,21 @@ SKYWAYS = {
     'searanch_to_pitchfork': {
         'points': [(-294, 61, 166), (-261, 61, 216)],
     },
-
-
-
-    # 'harbor onramp': {
-    #     'points': [(288, 63, -477), (288, 105, -520)]
-    # },
-    # 'beehive onramp': {
-    #     'points': [(1028, 79, -544), (1028, 105, -520)]
-    # },
-    # 'Harbor Rd': {
-    #     'points': [(0, 105, -520), (1028, 105, -520)],
-    #     'show_label': True,
-    #     'label_offset': (30, 6.5)
-    # },
-
-    # 'Snowy Rd': {
-    #     'points': [(-2544, 105, -350), (0, 105, -350)],
-    #     'show_label': True,
-    #     'label_fontsize': '+3',
-    #     'label_offset': (0, 8)
-    # },
-    # 'snowy village onramp': {
-    #     'points': [(-1854, 63, -289), (-1854, 105, -350)],
-    # },
-    # 'snowy tunnel 1': {
-    #     'points': [(-1392, 105, -350), (-836, 105, -350)],
-    #     'is_tunnel': True
-    # },
-    # 'snowy tunnel 2': {
-    #     'points': [(-1657, 105, -350), (-1578, 105, -350)],
-    #     'is_tunnel': True,
-    # },
-    # 'snowy tunnel 3': {
-    #     'points': [(-2177, 105, -350), (-1991, 105, -350)],
-    #     'is_tunnel': True,
-    # },
-
-    # 'Homeshack Rd': {
-    #     'points': [(0, 105, -520), (0, 105, 100), (-74, 105, 100)],
-    #     'show_label': True,
-    #     'label_fontsize': '-2',
-    #     'label_offset': (8, 20),
-    #     'label_rotation': 90
-    # },
-    # 'Homeshack Rd Close': {
-    #     'points': [(-88, 106, 87), (-223, 106, 87)],
-    # },
-
-    # 'Boathouse Rd': {
-    #     'points': [(-749, 105, -350), (-749, 105, 252)],
-    #     'show_label': True,
-    #     'label_offset': (0, 15),
-    #     'label_fontsize': '-2'
-    # },
-    # 'homeboat': {  # unnamed random road between home base and the boathouse
-    #     'points': [(-224, 105, 0), (-949, 105, 0)],
-    # },
-
-    # 'Lumberjack Ln': {
-    #     'points': [(-229, 113, 208), (-355, 113, 208)],
-    #     'show_label': True,
-    #     'label_fontsize': '-2',
-    #     'label_offset': (-10, -10)
-    # },
-    # 'lumberjack onramp': {
-    #     'points': [(-355, 113, 208), (-407, 63, 208)],
-    #     'show_label': False
-    # },
-
-    # 'Hill Village Rd': {
-    #     'points': [(-228, 110, 94), (-228, 113, 595), (-1200, 113, 595)],
-    #     'show_label': True,
-    #     'label_offset': (-35, 7)
-    # },
-    # 'Hill\nVillage\nRoad\nNorth': {
-    #     'points': [(-224, 105, 96), (-224, 105, -350)],
-    #     'show_label': True,
-    #     'label_fontsize': '-3.5',
-    #     'label_offset': (6, 2)
-    # },
-    # 'hill tunnel 1': {
-    #     'points': [(-927, 113, 595), (-970, 113, 595)],
-    #     'is_tunnel': True,
-    # },
-    # 'hill tunnel 2': {
-    #     'points': [(-987, 113, 595), (-1200, 113, 595)],
-    #     'is_tunnel': True,
-    # },
-
-    # 'Sea Ranch Rd': {
-    #     'points': [(-2331, 63, 1334), (-2331, 105, 1292), (-2331, 105, 1200), (-2000, 105, 1200), (-2000, 105, 600), (-1192, 105, 600)],
-    #     'show_label': True,
-    #     'label_offset': (40, 70)
-    # },
-    # 'sea ranch tunnel 1': {
-    #     'points': [(-1192, 105, 600), (-1248, 105, 600)],
-    #     'is_tunnel': True,
-    # },
-    # 'sea ranch tunnel 2': {
-    #     'points': [(-1614, 105, 600), (-1799, 105, 600)],
-    #     'is_tunnel': True,
-    # },
-
-    # 'Zombie Rd': {
-    #     'points': [(-2336, 105, 1229), (-2336, 105, -350)],
-    #     'show_label': True,
-    #     'label_rotation': 90,
-    #     'label_offset': (9, 0)
-    # },
+    'coarsegold_to_harbor_beehive_midpoint': {
+        'points': [(102, 60, 11), (88, 40, -66)],
+    },
+    'corsegold_to_pillville': {
+        'points': [(102, 60, 11), (83, 66, 114)],
+    },
+    'pillville_to_twin_cities_junction': {
+        'points': [(83, 66, 114), (83, 67, 261)],
+    },
+    'hill_village_to_twin_cities_1':  {
+        'points': [(-162, 70, 75), (83, 67, 261)],
+    },
+    'hill_village_to_twin_cities_2':  {
+        'points': [(83, 67, 261), (316, 78, 549)],
+    },
 }
 
 # this section should not be needed because they should all be defaults
